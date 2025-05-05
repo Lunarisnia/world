5ff80a0988b1bd29a81d258e5fda7dc2652929b6
Lounarisnia <r.arswendo.r@gmail.com>
2025-05-05 11:06:48 +0700

    wip: homepage grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,8 @@
 </script>
 
 <PersonalHeader></PersonalHeader>
+<div class="grid grid-cols-4 grid-rows-2 gap-4 min-h-screen bg-slate-800">
+	<div class="col-span-3 row-span-3 bg-red-100">01</div>
+	<div class="col-span-1 bg-red-100">01</div>
+	<div class="row-span-1 bg-red-100">01</div>
+</div>
