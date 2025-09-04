4495b61d7af0cc5c9d4703d250488867911e8f10
Lounarisnia <r.arswendo.r@gmail.com>
2025-09-04 13:12:45 +0700

    replaced the project for the millions of times

--- src/components/header/HeaderContent.svelte
+++
@@ -1,7 +0,0 @@
-<div class="">
-	<h1
-		class="text-stone-200 text-3xl font-mono font-thin hover:text-white hover:-translate-y-1 duration-100"
-	>
-		<slot />
-	</h1>
-</div>
--- src/components/sidemenu/SidebarHeader.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	let { title } = $props();
-</script>
-
-<div class="pt-10 ps-7 pb-10">
-	<div class="font-mono text-4xl font-bold text-white">
-		<h1>{title}</h1>
-	</div>
-</div>
