d3ad54001b178b7c35775509fded4fa84dfee4dd
Lounarisnia <r.arswendo.r@gmail.com>
2025-09-06 17:12:45 +0700

    wip: object generation with shader

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-import * as THREE from 'three';
 import Game from './src/game';
-import world from './src/world';
 
 const game = new Game();
 game.run();
--- src/world.js
+++ src/world.js
@@ -1,5 +1,7 @@
-import { BoxGeometry, Mesh, MeshBasicMaterial } from "three";
+import { BoxGeometry, Mesh, MeshBasicMaterial, ShaderMaterial } from "three";
 import Game from "./game";
+import basicVert from "/shaders/basic/basic.vert?url&raw"
+import redColor from "/shaders/basic/red-color.frag?url&raw"
 
 
 export default class World {
@@ -13,16 +15,18 @@ export default class World {
 	constructor(game) {
 		this.game = game;
 		const geometry = new BoxGeometry(1, 1, 1);
-		const material = new MeshBasicMaterial({ color: 0x00ff00 });
+		const material = new ShaderMaterial({
+			vertexShader: basicVert,
+			fragmentShader: redColor,
+		});
 		this.cube = new Mesh(geometry, material);
 		game.scene.add(this.cube);
 
 		game.mainCamera.instance.position.z = 5;
-		game.mainCamera.instance.position.y = 1;
 	}
 
 	update() {
-		this.cube.position.x = Math.sin(this.game.clock.getElapsedTime());
-		this.cube.position.y = Math.cos(this.game.clock.getElapsedTime());
+		this.cube.position.x = Math.cos(this.game.clock.getElapsedTime());
+		this.cube.position.y = Math.sin(this.game.clock.getElapsedTime());
 	}
 };
