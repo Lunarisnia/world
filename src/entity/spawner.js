5fa2ce1f303c2e3ac05d07efb9c2ea0b643db476
Lounarisnia <r.arswendo.r@gmail.com>
2025-09-12 00:22:18 +0700

    feat: cube function

--- src/entity/spawner.js
+++ src/entity/spawner.js
@@ -1,4 +1,4 @@
-import { ShaderMaterial, SphereGeometry } from "three";
+import { BoxGeometry, ShaderMaterial, SphereGeometry } from "three";
 import Entity from "./entity";
 
 export default class Spawner {
@@ -13,6 +13,19 @@ export default class Spawner {
 			fragmentShader,
 			uniforms,
 		});
+		return new Entity(geometry, material);
+	}
+
+	static CreateCubeWithShaderMaterial({ width, height, depth, widthSegments, heightSegments, depthSegments }, {
+		vertexShader, fragmentShader, uniforms,
+	}) {
+		const geometry = new BoxGeometry(width, height, depth, widthSegments, heightSegments, depthSegments);
+		const material = new ShaderMaterial({
+			vertexShader,
+			fragmentShader,
+			uniforms,
+		});
+
 		return new Entity(geometry, material);
 	}
 };
