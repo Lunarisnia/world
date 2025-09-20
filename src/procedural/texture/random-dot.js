872745183eb5d73c25ff06deac1f56c07b619576
Lounarisnia <r.arswendo.r@gmail.com>
2025-09-20 19:57:30 +0700

    wip: procedural canvas texture

---
+++ src/procedural/texture/random-dot.js
@@ -0,0 +1,40 @@
+export default class RandomDot {
+	// TODO: Convert this to a proper class
+	// 1. It has to keep references to the canvas
+	// 2. canvas does not have to be injected to the body
+	// 3. As long as it has a method or return CanvasTexture it should work
+
+	//const ctx = document.createElement('canvas').getContext('2d');
+	//document.body.appendChild(ctx.canvas);
+	//ctx.canvas.width = 256;
+	//ctx.canvas.height = 256;
+	//ctx.fillStyle = '#FFF';
+	//ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	//
+	//function randInt(min, max) {
+	//	if (max === undefined) {
+	//		max = min;
+	//		min = 0;
+	//	}
+	//	return Math.random() * (max - min) + min | 0;
+	//}
+	//
+	//function drawRandomDot() {
+	//	ctx.fillStyle = `#${randInt(0x1000000).toString(16).padStart(6, '0')}`;
+	//
+	//	ctx.beginPath();
+	//
+	//	const x = randInt(256);
+	//	const y = randInt(256);
+	//	const radius = randInt(10, 64);
+	//	ctx.arc(x, y, radius, 0, Math.PI * 2);
+	//	ctx.fill();
+	//}
+	//
+	//function render() {
+	//	drawRandomDot();
+	//	requestAnimationFrame(render);
+	//}
+	//requestAnimationFrame(render);
+
+}
