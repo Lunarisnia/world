8311b3edf513d9eabaa0396b1313afd7e577f179
Lounarisnia <r.arswendo.r@gmail.com>
2025-09-23 13:44:26 +0700

    feat: integrated gsap and spinner is now animated

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import Game from './src/game';
+import { gsap } from "gsap";
 
+import { CustomEase } from "gsap/CustomEase";
+
+gsap.registerPlugin(CustomEase);
 
 const game = new Game();
 game.run();
--- src/components/GroundBorder.js
+++ src/components/GroundBorder.js
@@ -7,6 +7,8 @@ import Entity from "../entity/entity";
 import Game from "../game";
 import GroundBorderMaterial from "../material/GroundBorderMaterial";
 import { Mesh } from "three";
+import gsap from "gsap";
+import CustomEase from "gsap/CustomEase";
 
 export default class GroundBorder extends Component {
 	constructor() {
@@ -21,10 +23,10 @@ export default class GroundBorder extends Component {
 		entity.addComponent(this);
 
 		const innerGeom = new GroundBorderGeometry(this.width - 1.0, this.height - 1.0, this.thickness);
-		this.innerMaterial = new GroundBorderMaterial();
-		entity.mesh.add(new Mesh(innerGeom, this.innerMaterial));
-		// TODO: I want the word open in the middle
-		// TOOD: I want the enter key icon floating on the middle of the box
+		const innerMaterial = new GroundBorderMaterial();
+		this.spinner = new Mesh(innerGeom, innerMaterial)
+		this.spinner.position.z = -1;
+		entity.mesh.add(this.spinner);
 
 		entity.mesh.position.y = -0.49;
 		entity.mesh.position.z = -4;
@@ -44,15 +46,23 @@ export default class GroundBorder extends Component {
 	init() {
 	}
 
+	// TODO: I want the word open in the middle
+	// TODO: I want the enter key icon floating on the middle of the box
 	update() {
 		this.mesh.material.uniforms.uTime.value = Game.instance.clock.getElapsedTime();
-		this.innerMaterial.uniforms.uTime.value = Game.instance.clock.getElapsedTime();
+		this.spinner.material.uniforms.uTime.value = Game.instance.clock.getElapsedTime();
 	}
 
-	onTriggerEnter() {
+	async onTriggerEnter() {
+		gsap.fromTo(this.spinner.scale, { x: -1.0, y: -1.0 }, { y: 1.0, x: 1.0, ease: "power1.out", duration: 0.5 });
+		gsap.fromTo(this.spinner.position, { z: -1 }, { z: 0.5, ease: "power1.inOut", duration: 0.5 });
 	}
 
-	onTriggerExit() {
-
+	async onTriggerExit() {
+		const timeline = gsap.timeline();
+		await gsap.fromTo(this.spinner.position, { z: 0.5 }, { z: 1.0, ease: "power1.inOut", duration: 0.4 })
+		gsap.to(this.spinner.position, { z: -1.0, ease: "power1.out", duration: 0.8 });
+		timeline.fromTo(this.spinner.scale, { x: 1.0, y: 1.0 }, { y: -1.0, x: -1.0, ease: "power1.out", duration: 0.8 })
+			.to(this.spinner.scale, { y: 1.0, x: 1.0, ease: "power1.out", duration: 0.1 });
 	}
 }
