f5b01359ec1967cf10f74398214b8728af0e8252
Lounarisnia <r.arswendo.r@gmail.com>
2025-10-07 14:40:00 +0700

    feat: center piece

--- src/components/CenterPiece.js
+++ src/components/CenterPiece.js
@@ -1,8 +1,11 @@
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import Component from "../component";
 import Entity from "../entity/entity";
 import Game from "../game";
 import CenterPieceGeometry from "../geometry/CenterPieceGeometry";
 import CenterPieceMaterial from "../material/CenterPieceMaterial";
+import SimpleMeshMaterial from "../material/SimpleMeshMaterial";
+import { Color } from "three";
 
 export default class CenterPiece extends Component {
 	constructor() {
@@ -13,12 +16,28 @@ export default class CenterPiece extends Component {
 		const entity = new Entity(geom, material);
 		entity.addComponent(this);
 
+		const objLoader = new OBJLoader();
+		const podiumMaterial = new SimpleMeshMaterial({
+			color: new Color(0.9, 0.9, 0.9),
+		});
+		objLoader.load("/models/CenterPiecePodium/CenterPiecePodium.obj", (root) => {
+			root.scale.setScalar(0.5);
+			root.position.y = -3;
+			root.traverse((child) => {
+				if (child.isMesh) {
+					child.material = podiumMaterial;
+				}
+			});
+
+			entity.mesh.add(root);
+		});
+
 		return entity;
 	}
 
 	init() {
 		this.mesh.position.x = 4;
-		this.mesh.position.y = 1;
+		this.mesh.position.y = 3;
 	}
 
 	update() {
--- src/geometry/WorkdDisplayGeometry.js
+++ src/geometry/WorkdDisplayGeometry.js
@@ -3,7 +3,6 @@ import { BoxGeometry } from "three";
 export default class WorkDisplayGeometry {
 	constructor() {
 
-		// TODO: Load the model here
 		const geom = new BoxGeometry(1, 1, 1);
 
 		return geom;
